9a543492c6ad7c6b6382b3bef6f4c59966f49ee1
Diego Freniche <diego@freniche.com>
2021-05-18 12:33:55 +0200

    Initial commit

---
+++ GithubActionsDemo/GithubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsDemoApp.swift
+//  GithubActionsDemo
+//
+//  Created by Diego Freniche Brito on 18/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
